b03003ec76b1a76c4e4733a2746c06b836da998d
RameshReddy7 <rameshr@newtglobal.com>
2016-05-12 15:28:32 +0530

    Update App.java

--- MavenJenkins/src/main/java/JenkinsDemo/MavenJenkins/App.java
+++ MavenJenkins/src/main/java/JenkinsDemo/MavenJenkins/App.java
@@ -10,6 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         
+        System.out.println("Ramesh Reddy");
+        
         
         System.out.println("Hai");
     }
